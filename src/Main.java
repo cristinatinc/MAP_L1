7bfd3ff4f1db49cf94e203154f8ee1ae7c42283b
Cristina <cristina.oana237@gmail.com>
2024-10-13 21:12:02 +0300

    Called the methods from ElectronicShop

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import static Aufgabe1.GradeCalculator.*;
 import static Aufgabe2.Array.*;
 import static Aufgabe3.BigNumber.*;
+import static Aufgabe4.ElectronicShop.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +27,7 @@ public class Main {
         // Aufgabe 2
         System.out.println();
         System.out.println("Aufgabe 2: ");
+
         int[] array = {4, 8, 3, 10, 17};
 
         int max = getMax(array);
@@ -59,6 +61,45 @@ public class Main {
         System.out.println("[2 3 6 0 0 0 0 0 0] * 2 = " + Arrays.toString(product));
         int[] quotient = divide(nr5, 2);
         System.out.println("[2 3 6 0 0 0 0 0 0] / 2 = " + Arrays.toString(quotient));
+
+        // Aufgabe 4
+        System.out.println();
+        System.out.println("Aufgabe 4: ");
+
+        int[] Tastatur = {40, 35, 70, 15, 45};
+        int cheapestKeyboard = getCheapestKeyboard(Tastatur);
+        System.out.println("Billigste Tastatur: " + cheapestKeyboard);
+
+        int[] Tastatur1 = {15, 20, 10, 35};
+        int[] USB = {20, 15, 40, 15};
+        int mostExpensiveItem = getMostExpensiveItem(Tastatur1, USB);
+        System.out.println("Teuerste Gegenstand: " + mostExpensiveItem);
+
+        int[] USB1 = {15, 45, 20};
+        int budgetMarkus = 30;
+        int usbOnABudget = getMostExpensiveUSBOnBudget(USB1, budgetMarkus);
+        System.out.println("Teuerste USB Laufwerk unter dem Budget : " + usbOnABudget);
+
+        int budget = 60;
+        // Bsp1
+        int[] tastaturen1 = {40, 50, 60};
+        int[] usb1 = {8, 12};
+        System.out.println("Maximalen Geldbetrag Beispiel 1: " + getMaxSpendableAmount(tastaturen1, usb1, budget));
+
+
+        // Bsp2
+        int[] keyboards2 = {60};
+        int[] usb2 = {8, 12};
+        System.out.println("Maximalen Geldbetrag Beispiel 2: " + getMaxSpendableAmount(keyboards2, usb2, budget));
+
+        // Bsp3
+        int[] keyboards3 = {40, 60};
+        int[] usb3 = {8, 12};
+        System.out.println("Maximalen Geldbetrag Beispiel 3: " + getMaxSpendableAmount(keyboards3, usb3, budget));
+
+
+
+
 
     }
 }
