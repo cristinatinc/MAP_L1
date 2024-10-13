d1af228d802ccbccb494372ad63d24fa1080a87f
Cristina <cristina.oana237@gmail.com>
2024-10-13 18:47:40 +0300

    Added methods to GradeCalculator and called them in Main
    methods added:
    -getAverageGrade
    -getRoundedOffGrades
    -maxRoundedGrade

--- src/Aufgabe1/GradeCalculator.java
+++ src/Aufgabe1/GradeCalculator.java
@@ -6,6 +6,40 @@ public class GradeCalculator {
         return Arrays.stream(grades).filter(grade -> grade < 40).toArray();
     }
 
+    public static double getAverageGrade(int[] grades) {
+        if (grades.length == 0) {
+            return 0.00;
+        }
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        double average =  sum / grades.length;
+        return Math.round(average * 100.00) / 100.00;
+    }
+
+    public static int[] getRoundedOffGrades(int[] grades) {
+        int[] roundedOffGrades = new int[grades.length];
+        for(int i = 0; i < grades.length; i++) {
+            if(grades[i] < 38) roundedOffGrades[i] = grades[i];
+            else{
+                int nextMultipleOfFive = ((grades[i] / 5) + 1) * 5;
+
+                if (nextMultipleOfFive - grades[i] < 3) {
+                    roundedOffGrades[i] = nextMultipleOfFive;
+                } else {
+                    roundedOffGrades[i] = grades[i];
+                }
+            }
+        }
+        return roundedOffGrades;
+    }
+
+    public static int maxRoundedGrade(int[] grades) {
+        int[] roundedOffGrades = getRoundedOffGrades(grades);
+        return Arrays.stream(roundedOffGrades).max().orElse(0);
+    }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-import static Aufgabe1.GradeCalculator.getInsufficientGrades;
+import static Aufgabe1.GradeCalculator.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +8,15 @@ public class Main {
         int[] grades = {29, 37, 38, 41, 84, 67};
         int[] insufficientGrades = getInsufficientGrades(grades);
         System.out.println("Die nicht ausreichende Noten aus [29, 37, 38, 41, 84, 67] sind: " + Arrays.toString(insufficientGrades));
+
+        double averageGrade = getAverageGrade(grades);
+        System.out.println("Die Mittelnote ist " + averageGrade);
+
+        int[] roundedOffGrades = getRoundedOffGrades(grades);
+        System.out.println("Die abgerundeten Noten sind: " + Arrays.toString(roundedOffGrades));
+
+        int maxGrade = maxRoundedGrade(grades);
+        System.out.println("Maximale abgerundete Note ist " + maxGrade);
+
     }
 }
